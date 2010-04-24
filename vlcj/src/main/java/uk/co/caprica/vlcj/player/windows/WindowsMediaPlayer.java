b8105cd6776d1803ec7542ecba70a75398d7e0d3
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-04-24 14:27:00 +0000

    Set the video surface for the Windows media player implementation.

--- vlcj/src/main/java/uk/co/caprica/vlcj/player/windows/WindowsMediaPlayer.java
+++ vlcj/src/main/java/uk/co/caprica/vlcj/player/windows/WindowsMediaPlayer.java
@@ -5,6 +5,9 @@ import java.awt.Canvas;
 import uk.co.caprica.vlcj.binding.internal.libvlc_media_player_t;
 import uk.co.caprica.vlcj.player.MediaPlayer;
 
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
 public class WindowsMediaPlayer extends MediaPlayer {
 
   public WindowsMediaPlayer(String[] args) {
@@ -13,6 +16,9 @@ public class WindowsMediaPlayer extends MediaPlayer {
 
   @Override
   protected void nativeSetVideoSurface(libvlc_media_player_t mediaPlayerInstance, Canvas videoSurface) {
-    throw new UnsupportedOperationException("Send patches!");
+    // Thanks for patch from BraCa
+    long drawable = Native.getComponentID(videoSurface);
+    Pointer ptr = Pointer.createConstant(drawable);
+    libvlc.libvlc_media_player_set_hwnd(mediaPlayerInstance, ptr);
   }
 }
