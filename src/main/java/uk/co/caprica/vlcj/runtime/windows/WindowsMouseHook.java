5dcb11b6955a4f303c105a0ea6c1cee2324ead68
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-05-09 13:26:35 +0000

    Sigh.

--- src/main/java/uk/co/caprica/vlcj/runtime/windows/WindowsMouseHook.java
+++ src/main/java/uk/co/caprica/vlcj/runtime/windows/WindowsMouseHook.java
@@ -205,7 +205,7 @@ public class WindowsMouseHook implements LowLevelMouseProc {
     
     HHOOK hook = getHook();
     if(hook != null) {
-      USER32_INSTANCE.UnhookWindowsHookEx(hHook);
+//      USER32_INSTANCE.UnhookWindowsHookEx(hHook);
       hook = null;
       
       // TODO ordinarily I'd interrupt the thread to force it to exit if it's
