1ed5f18f8bc2c31a3cf879075cb199246d5d9727
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-05-18 16:58:24 +0000

    Javadoc.

--- src/main/java/uk/co/caprica/vlcj/player/MediaPlayer.java
+++ src/main/java/uk/co/caprica/vlcj/player/MediaPlayer.java
@@ -454,6 +454,8 @@ public abstract class MediaPlayer {
   
   /**
    * Set the video play-back rate.
+   * <p>
+   * Some media protocols are not able to change the rate.
    * 
    * @param rate rate, where 1.0 is normal speed, 0.5 is half speed, 2.0 is double speed and so on
    */
