d8c34c175028e4a61f804d057fca35abf885d9a0
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-07-19 17:03:22 +0000

    Updated comment to match libvlc header.

--- vlcj/src/main/java/uk/co/caprica/vlcj/binding/LibVlc.java
+++ vlcj/src/main/java/uk/co/caprica/vlcj/binding/LibVlc.java
@@ -719,9 +719,7 @@ public interface LibVlc extends Library {
    * no effects. The specified identifier must correspond to an existing
    * Input/Output class X11 window. Pixmaps are <b>not</b> supported. The caller
    * shall ensure that the X11 server is the same as the one the VLC instance
-   * has been configured with. If XVideo is <b>not</b> used, it is assumed that
-   * the drawable has the following properties in common with the default X11
-   * screen: depth, scan line pad, black pixel. This is a bug.
+   * has been configured with.
    * 
    * @param p_mi the Media Player
    * @param drawable the ID of the X window
