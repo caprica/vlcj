389b3740ef4131ac737c977bf9e8e3813f0dd4bf
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-07-24 07:51:25 +0000

    Added very simple example multi-instance player.

---
+++ vlcj/src/test/java/uk/co/caprica/vlcj/test/multi/TestMultiPlayer.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2009, 2010 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.test.multi;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.DefaultFullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
+
+/**
+ * Example multi-instance player.
+ */
+public class TestMultiPlayer {
+
+  private String[] medias = {
+    "/home/movie/1.mp4", 
+    "/home/movie/2.mp4", 
+    "/home/movie/3.mp4", 
+    "/home/movie/4.mp4" 
+  };
+    
+  private int rows = 1;
+  private int cols = 4;
+  
+  private Frame mainFrame;
+  
+  private List<PlayerInstance> players = new ArrayList<PlayerInstance>();
+
+  private MediaPlayerFactory factory;
+  
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new TestMultiPlayer().start();
+      }
+    });
+  }
+  
+  public TestMultiPlayer() {
+    JPanel contentPane = new JPanel();
+    contentPane.setBackground(Color.black);
+    contentPane.setLayout(new GridLayout(rows, cols, 16, 16));
+    contentPane.setBorder(new EmptyBorder(16, 16, 16, 16));
+    
+    mainFrame = new Frame("VLCJ Test Multi Player for VLC 1.1.x");
+    
+    mainFrame.setLayout(new BorderLayout());
+    mainFrame.setBackground(Color.black);
+    mainFrame.add(contentPane, BorderLayout.CENTER);
+    mainFrame.setBounds(100, 100, 1600, 300);
+    mainFrame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent evt) {
+        for(PlayerInstance pi : players) {
+          pi.mediaPlayer().release();
+        }
+        factory.release();
+        System.exit(0);
+      }
+    });
+
+    mainFrame.addKeyListener(new KeyAdapter() {
+
+      @Override
+      public void keyPressed(KeyEvent e) {
+        for(int i = 0; i < players.size(); i++) {
+          players.get(i).mediaPlayer().pause();
+        }
+      }
+    });
+    
+    String[] args = {};
+    factory = new MediaPlayerFactory(args);
+    
+    FullScreenStrategy fullScreenStrategy = new DefaultFullScreenStrategy(mainFrame);
+    
+    for(int i = 0; i < medias.length; i++) {
+      EmbeddedMediaPlayer player = factory.newMediaPlayer(fullScreenStrategy);
+      PlayerInstance playerInstance = new PlayerInstance(player);
+      players.add(playerInstance);
+      
+      JPanel playerPanel = new JPanel();
+      playerPanel.setLayout(new BorderLayout());
+      playerPanel.setBorder(new LineBorder(Color.white, 2));
+      playerPanel.add(playerInstance.videoSurface());
+      
+      contentPane.add(playerPanel);
+    }
+    
+    mainFrame.setVisible(true);
+  }
+  
+  private void start() {
+    // There is a race condition somewhere when invoking libvlc_media_player_play()
+    // that causes a hard-failure and a fatal VM crash
+    //
+    // Sleeping for a while seems to get around it
+    //
+    // May need to look at somehow syncing somewhere...
+    //
+    // The more instances you have, the more you have to stagger the play calls
+    for(int i = 0; i < medias.length; i++) {
+      players.get(i).mediaPlayer().setVideoSurface(players.get(i).videoSurface());
+      players.get(i).mediaPlayer().prepareMedia(medias[i]);
+    }
+
+    for(int i = 0; i < medias.length; i++) {
+      players.get(i).mediaPlayer().play();
+      
+      // If you like seeing virtual machine crashes, remove the sleep...
+      try {
+        Thread.sleep(500);
+      } 
+      catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
