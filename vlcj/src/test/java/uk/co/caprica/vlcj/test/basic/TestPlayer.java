79810790ec597ce5da3cbfda7b39240ba0c90a49
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-08-21 08:06:12 +0000

    Added an example (non-functioning) menu to test interaction between menu and video surface.

--- vlcj/src/test/java/uk/co/caprica/vlcj/test/basic/TestPlayer.java
+++ vlcj/src/test/java/uk/co/caprica/vlcj/test/basic/TestPlayer.java
@@ -47,7 +47,11 @@ import java.util.List;
 import java.util.regex.Matcher;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
@@ -93,6 +97,7 @@ public class TestPlayer {
   private MediaPlayerFactory mediaPlayerFactory;
 
   private Log log;
+  private LogHandler logHandler;
   
   private EmbeddedMediaPlayer mediaPlayer;
   
@@ -176,7 +181,7 @@ public class TestPlayer {
     // Create a new log handler to display the native libvlc log
     log = mediaPlayerFactory.newLog();
     log.setThreshold(LogLevel.DBG);
-    new LogHandler(log, 1000)
+    logHandler = new LogHandler(log, 1000)
       // Add a log message handler to send the native log messages to the local log
       .addLogMessageHandler(new DefaultLogMessageHandler())
       // Add a log message handler to search for media opening error messages in the native log
@@ -186,7 +191,8 @@ public class TestPlayer {
           Logger.error("failed to open filename {}", matcher.group(1));
         }
       })
-    ).start();
+    );
+    logHandler.start();
     
     mediaPlayer = mediaPlayerFactory.newMediaPlayer(fullScreenStrategy);
 
@@ -198,16 +204,26 @@ public class TestPlayer {
     mainFrame.add(videoSurface, BorderLayout.CENTER);
     mainFrame.add(controlsPanel, BorderLayout.SOUTH);
     mainFrame.add(videoAdjustPanel, BorderLayout.EAST);
+    mainFrame.setJMenuBar(buildMenuBar());
     mainFrame.pack();
     mainFrame.addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent evt) {
         Logger.debug("windowClosing(evt={})", evt);
 
+        if(logHandler != null) {
+          logHandler.release();
+          logHandler = null;
+        }
+        
         if(log != null) {
           log.close();
           log = null;
         }
 
+        if(videoSurface instanceof WindowsCanvas) {
+          ((WindowsCanvas)videoSurface).release();
+        }
+        
         if(mediaPlayer != null) {
           mediaPlayer.release();
           mediaPlayer = null;
@@ -233,6 +249,7 @@ public class TestPlayer {
             if(keyEvent.getKeyCode() == KeyEvent.VK_F12) {
               controlsPanel.setVisible(!controlsPanel.isVisible());
               videoAdjustPanel.setVisible(!videoAdjustPanel.isVisible());
+              mainFrame.getJMenuBar().setVisible(!mainFrame.getJMenuBar().isVisible());
               mainFrame.invalidate();
               mainFrame.validate();
             }
@@ -264,7 +281,7 @@ public class TestPlayer {
     Logger.debug("transparentWindowsSupport={}", transparentWindowsSupport);
     
     if(transparentWindowsSupport) {
-      final Window test = new Window(mainFrame, WindowUtils.getAlphaCompatibleGraphicsConfiguration()) {
+      final Window test = new Window(null, WindowUtils.getAlphaCompatibleGraphicsConfiguration()) {
         public void paint(Graphics g) {
           Graphics2D g2 = (Graphics2D)g;
   
@@ -290,6 +307,74 @@ public class TestPlayer {
 //	enableMousePointer(false);
   }
   
+  private JMenuBar buildMenuBar() {
+    // Menus are just added as an example of overlapping the video - they are 
+    // non-functional in this demo player
+    
+    JMenuBar menuBar = new JMenuBar();
+    
+    JMenu mediaMenu = new JMenu("Media");
+    mediaMenu.setMnemonic('m');
+    
+    JMenuItem mediaPlayFileMenuItem = new JMenuItem("Play File...");
+    mediaPlayFileMenuItem.setMnemonic('f');
+    mediaMenu.add(mediaPlayFileMenuItem);
+    
+    JMenuItem mediaPlayStreamMenuItem = new JMenuItem("Play Stream...");
+    mediaPlayFileMenuItem.setMnemonic('s');
+    mediaMenu.add(mediaPlayStreamMenuItem);
+    
+    mediaMenu.add(new JSeparator());
+    
+    JMenuItem mediaExitMenuItem = new JMenuItem("Exit");
+    mediaExitMenuItem.setMnemonic('x');
+    mediaMenu.add(mediaExitMenuItem);
+    
+    menuBar.add(mediaMenu);
+    
+    JMenu playbackMenu = new JMenu("Playback");
+    playbackMenu.setMnemonic('p');
+    
+    JMenu playbackChapterMenu = new JMenu("Chapter");
+    playbackChapterMenu.setMnemonic('c');
+    for(int i = 1; i <= 25; i++) {
+      JMenuItem chapterMenuItem = new JMenuItem("Chapter " + i);
+      playbackChapterMenu.add(chapterMenuItem);
+    }
+    playbackMenu.add(playbackChapterMenu);
+    
+    JMenu subtitlesMenu = new JMenu("Subtitles");
+    playbackChapterMenu.setMnemonic('s');
+    String[] subs = {"01 English (en)", "02 English Commentary (en)", "03 French (fr)", "04 Spanish (es)", "05 German (de)", "06 Italian (it)"};
+    for(int i = 0; i < subs.length; i++) {
+      JMenuItem subtitlesMenuItem = new JMenuItem(subs[i]);
+      subtitlesMenu.add(subtitlesMenuItem);
+    }
+    playbackMenu.add(subtitlesMenu);
+    
+    menuBar.add(playbackMenu);
+    
+    JMenu toolsMenu = new JMenu("Tools");
+    toolsMenu.setMnemonic('t');
+    
+    JMenuItem toolsPreferencesMenuItem = new JMenuItem("Preferences...");
+    toolsPreferencesMenuItem.setMnemonic('p');
+    toolsMenu.add(toolsPreferencesMenuItem);
+    
+    menuBar.add(toolsMenu);
+    
+    JMenu helpMenu = new JMenu("Help");
+    helpMenu.setMnemonic('h');
+    
+    JMenuItem helpAboutMenuItem = new JMenuItem("About...");
+    helpAboutMenuItem.setMnemonic('a');
+    helpMenu.add(helpAboutMenuItem);
+    
+    menuBar.add(helpMenu);
+    
+    return menuBar;
+  }
+
   private final class TestPlayerMediaPlayerEventListener extends MediaPlayerEventAdapter {
     @Override
     public void mediaChanged(MediaPlayer mediaPlayer) {
