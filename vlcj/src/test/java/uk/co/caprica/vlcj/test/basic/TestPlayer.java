24e7c3b718448825d4eeef7adc51b679049bcc62
Mark Lee <mark.lee@capricasoftware.co.uk>
2010-10-31 16:09:44 +0000

    Increase version number ready for next release.

--- vlcj/src/test/java/uk/co/caprica/vlcj/test/basic/TestPlayer.java
+++ vlcj/src/test/java/uk/co/caprica/vlcj/test/basic/TestPlayer.java
@@ -435,7 +435,7 @@ public class TestPlayer {
       }
 
       // Demo the marquee      
-      mediaPlayer.setMarqueeText("VLCJ 1.1.3");
+      mediaPlayer.setMarqueeText("VLCJ 1.1.4");
       mediaPlayer.setMarqueeSize(40);
       mediaPlayer.setMarqueeOpacity(95);
       mediaPlayer.setMarqueeColour(Color.white);
