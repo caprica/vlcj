64bd0e31139a2fe4f2d02218411f5ee388f81008
Mark Lee <mark.lee@capricasoftware.co.uk>
2011-03-13 08:25:03 +0000

    Provide an interface for Windows users to programmatically disable desktop compositing since this may cause problems with full-screen mode.

---
+++ vlcj/src/main/java/uk/co/caprica/vlcj/binding/LibDwmApi.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2009, 2010, 2011 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.binding;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+/**
+ * JNA interface to the native LibDwm (Desktop Window Manager) library.
+ * <p>
+ * This library is only available on Windows platforms.
+ * <p>
+ * The functionality provided by this library may be helpful especially when
+ * working with full-screen exclusive mode in Java which suffers from numerous
+ * idiosyncracies on Windows, some of which can be mitigated by ensuring that
+ * desktop window composition is disabled. Disabling composition can be done at
+ * the operating system level, but is more convenient for end-users if the
+ * application itself does it. 
+ */
+public interface LibDwmApi extends Library {
+
+  /**
+   * Native library instance. 
+   */
+  LibDwmApi INSTANCE = (LibDwmApi)Native.loadLibrary("dwmapi", LibDwmApi.class);
+
+  /**
+   * Disable composition.
+   */
+  public static final int DWM_EC_DISABLECOMPOSITION = 0;
+  
+  /**
+   * Enable composition.
+   */
+  public static final int DWM_EC_ENABLECOMPOSITION = 1;
+  
+  /**
+   * Enable/disable desktop window composition.
+   * 
+   * @param enable enable/disable, i.e. DWM_EC_ENABLECOMPOSITION or DWM_EC_DISABLECOMPOSITION 
+   */
+  void DwmEnableComposition(int enable);
+}
