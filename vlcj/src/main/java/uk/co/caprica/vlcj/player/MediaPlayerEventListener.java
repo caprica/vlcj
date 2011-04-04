4e09824e05a9f271dcde86a63cc7e575040051a8
Mark Lee <mark.lee@capricasoftware.co.uk>
2011-04-04 07:17:55 +0000

    Javadoc.

--- vlcj/src/main/java/uk/co/caprica/vlcj/player/MediaPlayerEventListener.java
+++ vlcj/src/main/java/uk/co/caprica/vlcj/player/MediaPlayerEventListener.java
@@ -24,208 +24,219 @@ import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 /**
  * Specification for a component that is interested in receiving event
  * notifications from the media player.
+ * <p>
+ * In most cases any event that fires for a media item will also fire for any
+ * sub-item, so for example mediaChanged() will fire for the "main" media item
+ * and each subsequent sub-item. 
  */
 public interface MediaPlayerEventListener {
 
   // === Events relating to the media player ==================================
 
   /**
+   * The media changed.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void mediaChanged(MediaPlayer mediaPlayer);
 
   /**
+   * Opening the media. 
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void opening(MediaPlayer mediaPlayer);
 
   /**
+   * Buffering media.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void buffering(MediaPlayer mediaPlayer);
 
   /**
+   * The media started playing.
+   * <p>
+   * There is no guarantee that a video output has been created at this point.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void playing(MediaPlayer mediaPlayer);
 
   /**
+   * Media paused.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void paused(MediaPlayer mediaPlayer);
 
   /**
+   * Media stopped.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void stopped(MediaPlayer mediaPlayer);
 
   /**
+   * Media skipped forward.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void forward(MediaPlayer mediaPlayer);
 
   /**
+   * Media skipped backward.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void backward(MediaPlayer mediaPlayer);
 
   /**
+   * Media finished playing (i.e. the end was reached without being stopped).
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void finished(MediaPlayer mediaPlayer);
   
   /**
+   * Media play-back time changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newTime
+   * @param mediaPlayer media player that raised the event
+   * @param newTime new time
    */
   void timeChanged(MediaPlayer mediaPlayer, long newTime);
 
   /**
+   * Media play-back position changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newPosition
+   * @param mediaPlayer media player that raised the event
+   * @param newPosition percentage between 0.0 and 1.0
    */
   void positionChanged(MediaPlayer mediaPlayer, float newPosition);
 
   /**
+   * Media seekable status changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newSeekable
+   * @param mediaPlayer media player that raised the event
+   * @param newSeekable new seekable status
    */
   void seekableChanged(MediaPlayer mediaPlayer, int newSeekable);
 
   /**
+   * Media pausable status changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newSeekable
+   * @param mediaPlayer media player that raised the event
+   * @param newSeekable new pausable status
    */
   void pausableChanged(MediaPlayer mediaPlayer, int newSeekable);
 
   /**
+   * Media title changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newSeekable
+   * @param mediaPlayer media player that raised the event
+   * @param newTitle new title
    */
-  void titleChanged(MediaPlayer mediaPlayer, int newSeekable);
+  void titleChanged(MediaPlayer mediaPlayer, int newTitle);
 
   /**
+   * A snapshot was taken.
    * 
-   * 
-   * @param mediaPlayer
-   * @param filename
+   * @param mediaPlayer media player that raised the event
+   * @param filename name of the file containing the snapshot
    */
   void snapshotTaken(MediaPlayer mediaPlayer, String filename);
 
   /**
+   * Media length changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newLength
+   * @param mediaPlayer media player that raised the event
+   * @param newLength new length (number of milliseconds)
    */
   void lengthChanged(MediaPlayer mediaPlayer, long newLength);
 
   /**
+   * An error occurred.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void error(MediaPlayer mediaPlayer);
 
   // === Events relating to the current media =================================
   
   /**
+   * Current media meta data changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param metaType 
+   * @param mediaPlayer media player that raised the event
+   * @param metaType type of meta data that changed 
    */
   void mediaMetaChanged(MediaPlayer mediaPlayer, int metaType);
   
   /**
+   * A new sub-item was added to the current media.
    * 
-   * 
-   * @param mediaPlayer
-   * @param subItem 
+   * @param mediaPlayer media player that raised the event
+   * @param subItem native sub-item handle 
    */
   void mediaSubItemAdded(MediaPlayer mediaPlayer, libvlc_media_t subItem);
   
   /**
+   * The current media duration changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newDuration
+   * @param mediaPlayer media player that raised the event
+   * @param newDuration new duration (number of milliseconds)
    */
   void mediaDurationChanged(MediaPlayer mediaPlayer, long newDuration);
   
   /**
+   * The current media parsed status changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newStatus
+   * @param mediaPlayer media player that raised the event
+   * @param newStatus new parsed status
    */
   void mediaParsedChanged(MediaPlayer mediaPlayer, int newStatus);
   
   /**
+   * The current media was freed.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void mediaFreed(MediaPlayer mediaPlayer);
 
   /**
+   * The current media state changed.
    * 
-   * 
-   * @param mediaPlayer
-   * @param newState
+   * @param mediaPlayer media player that raised the event
+   * @param newState new state
    */
   void mediaStateChanged(MediaPlayer mediaPlayer, int newState);
 
   // === Synthetic/semantic events ============================================
 
   /**
+   * New media was opened.
+   * <p>
+   * This is raised only for the "main" media item and not for any sub-items.
    * 
-   * 
-   * @param mediaPlayer
+   * @param mediaPlayer media player that raised the event
    */
   void newMedia(MediaPlayer mediaPlayer);
   
   /**
+   * A sub-item was played.
+   * <p>
+   * There is no guarantee the sub-item actually started, but it was at least
+   * attempted to be played.
    * 
-   * 
-   * @param mediaPlayer
-   * @param subItemIndex
+   * @param mediaPlayer media player that raised the event
+   * @param subItemIndex index of the sub-item that was played
    */
   void subItemPlayed(MediaPlayer mediaPlayer, int subItemIndex);
   
   /**
+   * A sub-item finished playing.
    *
-   *
-   * @param mediaPlayer
-   * @param subItemIndex
+   * @param mediaPlayer media player that raised the event
+   * @param subItemIndex index of the sub-item that finished playing
    */
   void subItemFinished(MediaPlayer mediaPlayer, int subItemIndex);
 }
