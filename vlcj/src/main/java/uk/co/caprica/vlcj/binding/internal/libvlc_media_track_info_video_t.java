c972756f7c67f524465bdb568153ac79ef35571d
Mark Lee <mark.lee@capricasoftware.co.uk>
2011-05-14 10:03:08 +0000

    Add ByValue.

--- vlcj/src/main/java/uk/co/caprica/vlcj/binding/internal/libvlc_media_track_info_video_t.java
+++ vlcj/src/main/java/uk/co/caprica/vlcj/binding/internal/libvlc_media_track_info_video_t.java
@@ -26,6 +26,8 @@ import com.sun.jna.Structure;
  */
 public class libvlc_media_track_info_video_t extends Structure {
 
+  public static class ByValue extends libvlc_media_track_info_video_t implements Structure.ByValue {}
+
   /* Video specific */
   public int i_height;
   public int i_width;
