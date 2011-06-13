1e4a017d61b1be94b55120d30e70e8684f5dc837
Mark Lee <mark.lee@capricasoftware.co.uk>
2011-06-13 08:38:58 +0000

    Added a new runtime method for Linux et al to ask the window manager to make a 'real' full-screen window - i.e. the window will be sized to the screen bounds and appear above panels and menus. This is implemented by low-level native X11 bindings.

--- vlcj/src/main/java/uk/co/caprica/vlcj/runtime/x/LibXUtil.java
+++ vlcj/src/main/java/uk/co/caprica/vlcj/runtime/x/LibXUtil.java
@@ -19,27 +19,48 @@
 
 package uk.co.caprica.vlcj.runtime.x;
 
+import java.awt.Window;
+
 import uk.co.caprica.vlcj.binding.LibX11;
 import uk.co.caprica.vlcj.logger.Logger;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
+import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+import com.sun.jna.platform.unix.X11;
+import com.sun.jna.platform.unix.X11.Display;
+import com.sun.jna.platform.unix.X11.XClientMessageEvent;
+import com.sun.jna.platform.unix.X11.XEvent;
+
 /**
- * A helper class that provides a method to safely try and initialise threads
- * for the LibX11 library.
+ * A helper class that provides various methods that may be useful when running
+ * on an X-Windows platform.
  * <p>
- * This can prevent some fatal native crashes on Linux and related operating
- * systems.
- * <p>
- * This class will fail <em>silently</em> if the initialisation fails, allowing
- * the application to continue normally where X is not available.
+ * There are two types of method provided by this class:
+ * <ul>
+ *   <li>Explicitly initialise the X-Windows system threads;</li>
+ *   <li>Interaction with the Window Manager itself.</li>
+ * </ul>
  */
 public class LibXUtil {
 
+  // X window message definitions
+  private static final int _NET_WM_STATE_REMOVE = 0;
+  private static final int _NET_WM_STATE_ADD    = 1;
+//  private static final int _NET_WM_STATE_TOGGLE = 2;
+  
+  // X boolean definitions
+  private static final int TRUE = 1;
+//  private static final int FALSE = 0;
+  
   /**
    * Attempt to initialise LibX threads.
    * <p>
    * It is safe to invoke this on any operating system and it will silently
    * fail if X is not supported.
+   * <p>
+   * This can prevent some fatal native crashes on Linux and related operating
+   * systems.
    * <p>
    * This <strong>should not</strong> be required, but in practice it may be
    * useful.
@@ -53,5 +74,86 @@ public class LibXUtil {
         Logger.trace("Did not initialise LibX11: {}", t.getMessage());
       }
     }
+  }
+  
+  /**
+   * Ask the window manager to make a window full-screen. 
+   * <p>
+   * This method sends a low-level event to an X window to request that the
+   * window be made 'real' full-screen - i.e. the window will be sized to fill
+   * the entire screen bounds, and will appear <em>above</em> any window 
+   * manager screen furniture such as panels and menus.
+   * <p>
+   * This method should only be called on platforms where X is supported.
+   * <p>
+   * The implementations makes use of the JNA X11 platform binding.
+   * 
+   * @param w window to make full-screen
+   * @param fullScreen <code>true</code> to make the window full-screen; <code>false</code> to restore the window to it's original size and position
+   * @return <code>true</code> if the message was successfully sent to the window; <code>false</code> otherwise
+   */
+  public static boolean setFullScreenWindow(Window w, boolean fullScreen) {
+    // Use the JNA platform X11 binding
+    X11 x = X11.INSTANCE;
+    Display display = null;
+    try {
+      // Open the display
+      display = x.XOpenDisplay(null);
+      // Send the message
+      int result = sendClientMessage(
+        display, 
+        Native.getWindowID(w), 
+        "_NET_WM_STATE", 
+        new NativeLong(fullScreen ? _NET_WM_STATE_ADD : _NET_WM_STATE_REMOVE), 
+        x.XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", false)
+      );
+      return result != 0;
+    }
+    finally {
+      if(display != null) {
+        // Close the display
+        x.XCloseDisplay(display);
+      }
+    }
+  }
+  
+  /**
+   * Helper method to send a client message to an X window.
+   * 
+   * @param display display
+   * @param wid native window identifier
+   * @param msg type of message to send
+   * @param data0 message data
+   * @param data1 message data
+   * @return <code>1</code> if the message was successfully sent to the window; <code>0</code> otherwise
+   */
+  private static int sendClientMessage(Display display, long wid, String msg, NativeLong data0, NativeLong data1) {
+    // Use the JNA platform X11 binding
+    X11 x = X11.INSTANCE;
+    // Create and populate a client-event structure
+    XEvent event = new XEvent();
+    event.type = X11.ClientMessage;
+    // Select the proper union structure for the event type and populate it
+    event.setType(XClientMessageEvent.class);
+    event.xclient.type = X11.ClientMessage;
+    event.xclient.serial = new NativeLong(0L);
+    event.xclient.send_event = TRUE;
+    event.xclient.message_type = x.XInternAtom(display, msg, false);
+    event.xclient.window = new com.sun.jna.platform.unix.X11.Window(wid);
+    event.xclient.format = 32;
+    // Select the proper union structure for the event data and populate it
+    event.xclient.data.setType(NativeLong[].class);
+    event.xclient.data.l[0] = data0;
+    event.xclient.data.l[1] = data1;
+    event.xclient.data.l[2] = new NativeLong(0L);
+    event.xclient.data.l[3] = new NativeLong(0L);
+    event.xclient.data.l[4] = new NativeLong(0L);
+    // Send the event
+    NativeLong mask = new NativeLong(X11.SubstructureRedirectMask | X11.SubstructureNotifyMask);
+    int result = x.XSendEvent(display, x.XDefaultRootWindow(display), 0, mask, event);
+    // Flush, since we're not processing an X event loop
+    x.XFlush(display);
+    // Finally, return the result of sending the event
+    return result;
   }
 }
