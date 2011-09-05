5451bf6d87a17fd0b0b08d7c1b302673002ec8db
Mark Lee <mark.lee@capricasoftware.co.uk>
2011-09-05 18:35:59 +0000

    Started adding some package-info.java for Javadoc.

---
+++ vlcj/src/main/java/uk/co/caprica/vlcj/binding/internal/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Internal JNA bindings for libvlc, bindings to native types and structures.
+ */
+package uk.co.caprica.vlcj.binding.internal;
---
+++ vlcj/src/main/java/uk/co/caprica/vlcj/binding/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the JNA bindings, and related classes, to use the libvlc native
+ * library.
+ */
+package uk.co.caprica.vlcj.binding;
