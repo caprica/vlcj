382a7da4a7baf71eccc190fff5baf67e4c036267
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-10-25 19:25:31 +0100

    Add missing package info for the new packages.

---
+++ src/main/java/uk/co/caprica/vlcj/player/condition/conditions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Default implementations for commonly needed media player conditions used
+ * with the synchronous programming framework.
+ */
+package uk.co.caprica.vlcj.player.condition.conditions;
---
+++ src/main/java/uk/co/caprica/vlcj/player/condition/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Synchronous programming framework.
+ * <p>
+ * Ordinarily media player programming requires an asynchronous approach, this
+ * small framework makes it easier to use a synchronous approach instead.
+ * <p>
+ * Most applications are not expected to use this synchronous approach, but
+ * there are some use-cases where it can be convenient.
+ */
+package uk.co.caprica.vlcj.player.condition;
