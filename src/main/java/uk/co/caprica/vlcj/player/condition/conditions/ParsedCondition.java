28b05c5f22b8dc731ff705c81973bd1c927cd2c7
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-11-11 20:17:49 +0000

    Add an implementation to wait for media parsed.

---
+++ src/main/java/uk/co/caprica/vlcj/player/condition/conditions/ParsedCondition.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009, 2010, 2011, 2012 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.player.condition.conditions;
+
+import uk.co.caprica.vlcj.logger.Logger;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.condition.DefaultCondition;
+
+/**
+ * Implementation of a condition that waits for the media player to report that
+ * media has been parsed.
+ */
+public class ParsedCondition extends DefaultCondition<Integer> {
+
+    /**
+     * Create a condition.
+     *
+     * @param mediaPlayer media player
+     */
+    public ParsedCondition(MediaPlayer mediaPlayer) {
+        super(mediaPlayer);
+    }
+
+    @Override
+    public final void mediaParsedChanged(MediaPlayer mediaPlayer, int newStatus) {
+        Logger.debug("mediaParsedChange(mediaPlayer={},newStatus={})", mediaPlayer, newStatus);
+        ready(newStatus);
+    }
+}
