ff9b3d4353ee69cd6d255f14057918edadf600f9
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-12-31 08:16:38 +0000

    Eliminate the overhead when creating non GPL builds.

--- src/main/java/uk/co/caprica/vlcj/Info.java
+++ src/main/java/uk/co/caprica/vlcj/Info.java
@@ -30,36 +30,6 @@ import uk.co.caprica.vlcj.version.Version;
 public class Info {
 
     /**
-     * Application name.
-     */
-    private static final String APP_MSG =
-        "       _       _"    + "\n" +
-        "__   _| | ___ (_)"   + "\n" +
-        "\\ \\ / / |/ __|| |" + "\n" +
-        " \\ V /| | (__ | |"  + "\n" +
-        "  \\_/ |_|\\___|/ | 2.2.0" + "\n" +
-        "            |__/  www.capricasoftware.co.uk" + "\n";
-
-    /**
-     * Application license (GPL3) summary text.
-     */
-    private static final String LICENSE_MSG =
-        "VLCJ is free software: you can redistribute it and/or modify"         + "\n" +
-        "it under the terms of the GNU General Public License as published by" + "\n" +
-        "the Free Software Foundation, either version 3 of the License, or"    + "\n" +
-        "(at your option) any later version."                                  + "\n" +
-        ""                                                                     + "\n" +
-        "VLCJ is distributed in the hope that it will be useful,"              + "\n" +
-        "but WITHOUT ANY WARRANTY; without even the implied warranty of"       + "\n" +
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"        + "\n" +
-        "GNU General Public License for more details."                         + "\n" +
-        ""                                                                     + "\n" +
-        "You should have received a copy of the GNU General Public License"    + "\n" +
-        "along with VLCJ.  If not, see <http://www.gnu.org/licenses/>."        + "\n" +
-        ""                                                                     + "\n" +
-        "Copyright 2009, 2010, 2011, 2012 Caprica Software Limited."           + "\n";
-
-    /**
      * Singleton holder.
      */
     private static class InfoHolder {
@@ -88,13 +58,6 @@ public class Info {
      * Private constructor.
      */
     private Info() {
-        // Suppress the banner and license message if the calling application explicitly
-        // sets this system property
-        if(!"yes".equalsIgnoreCase(System.getProperty("vlcj.accept.gpl3"))) {
-            System.err.println(APP_MSG);
-            System.err.println(LICENSE_MSG);
-            System.err.flush();
-        }
         try {
             Properties properties = new Properties();
             properties.load(getClass().getResourceAsStream("/build.properties"));
