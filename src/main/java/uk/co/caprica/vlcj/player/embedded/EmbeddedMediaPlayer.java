f0fb173b8f2b907f728bef64ebd62ba23d701221
Mark Lee <mark.lee@capricasoftware.co.uk>
2013-06-12 18:19:22 +0100

    Clarified Javadoc regarding mouse event handling.

--- src/main/java/uk/co/caprica/vlcj/player/embedded/EmbeddedMediaPlayer.java
+++ src/main/java/uk/co/caprica/vlcj/player/embedded/EmbeddedMediaPlayer.java
@@ -158,6 +158,10 @@ public interface EmbeddedMediaPlayer extends MediaPlayer {
      * <p>
      * It may be necessary on some platforms to invoke this method with a <code>false</code> parameter
      * value for Java mouse and keyboard listeners to work.
+     * <p>
+     * Note that clicking the video surface on certain platforms is not guaranteed to capture mouse
+     * events - it may be necessary to respond to a mouse pressed event on the video surface and
+     * explicitly request the input focus to the video surface.
      *
      * @param enable <code>true</code> to enable, <code>false</code> to disable
      */
