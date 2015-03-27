35e2f10fa5dc95219f9c0b9397c38382d3506bce
Mark Lee <mark.lee@capricasoftware.co.uk>
2015-03-27 18:01:36 +0000

    Add debug statement.

--- src/main/java/uk/co/caprica/vlcj/player/DefaultMediaPlayer.java
+++ src/main/java/uk/co/caprica/vlcj/player/DefaultMediaPlayer.java
@@ -1308,6 +1308,7 @@ public abstract class DefaultMediaPlayer extends AbstractMediaPlayer implements 
 
     @Override
     public String getCodecDescription(libvlc_track_type_t type, int codec) {
+        logger.debug("getCodecDescription(type={},codec={})", type, codec);
         if(LibVlcVersion.getVersion().atLeast(LibVlcVersion.LIBVLC_300)) {
             return libvlc.libvlc_media_get_codec_description(type.intValue(), codec);
         }
