12c6c70b3aa491ff014231ab22fa02462cf00038
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-01-27 12:16:34 +0000

    Remove the WindowsRuntimeUtil class
    The only function in that class was used to get the VLC install
    directory from the Windows Registry.
    
    That trivial function was in-lined directly in the related
    directory provider.

--- src/main/java/uk/co/caprica/vlcj/discovery/provider/WindowsInstallDirectoryProvider.java
+++ src/main/java/uk/co/caprica/vlcj/discovery/provider/WindowsInstallDirectoryProvider.java
@@ -19,10 +19,21 @@
 
 package uk.co.caprica.vlcj.discovery.provider;
 
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
-import uk.co.caprica.vlcj.runtime.windows.WindowsRuntimeUtil;
 
 public class WindowsInstallDirectoryProvider implements DiscoveryDirectoryProvider {
+
+    /**
+     * The VLC registry key, under HKLM.
+     */
+    private static final String VLC_REGISTRY_KEY = "SOFTWARE\\VideoLAN\\VLC";
+
+    /**
+     * The VLC registry key for the installation directory.
+     */
+    private static final String VLC_INSTALL_DIR_KEY = "InstallDir";
 
     @Override
     public int priority() {
@@ -31,13 +42,27 @@ public class WindowsInstallDirectoryProvider implements DiscoveryDirectoryProvid
 
     @Override
     public String[] directories() {
-        String installDir = WindowsRuntimeUtil.getVlcInstallDir();
+        String installDir = getVlcInstallDir();
         return installDir != null ? new String[] {installDir} : new String[0];
     }
 
     @Override
     public boolean supported() {
         return RuntimeUtil.isWindows();
+    }
+
+    /**
+     *
+     *
+     * @return
+     */
+    private String getVlcInstallDir() {
+        try {
+            return Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, VLC_REGISTRY_KEY, VLC_INSTALL_DIR_KEY);
+        }
+        catch(Exception e) {
+            return null;
+        }
     }
 
 }
--- src/main/java/uk/co/caprica/vlcj/runtime/windows/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides various utility classes for applications running in an MS-Windows
- * environment.
- */
-package uk.co.caprica.vlcj.runtime.windows;
