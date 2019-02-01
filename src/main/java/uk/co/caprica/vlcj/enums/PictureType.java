a072596e56bfb1ad9d0102e0664a60ab8a636dca
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-01 13:03:00 +0000

    Add enum classes for LibVlc 4.0.0 thumbnailer

---
+++ src/main/java/uk/co/caprica/vlcj/enums/PictureType.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2019 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Enumeration of picture types.
+ */
+public enum PictureType {
+
+    ARGB(0),
+    PNG (1),
+    JPG (2);
+
+    private static final Map<Integer, PictureType> INT_MAP = new HashMap<Integer, PictureType>();
+
+    static {
+        for(PictureType event : PictureType.values()) {
+            INT_MAP.put(event.intValue, event);
+        }
+    }
+
+    public static PictureType pictureType(int intValue) {
+        return INT_MAP.get(intValue);
+    }
+
+    private final int intValue;
+
+    PictureType(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int intValue() {
+        return intValue;
+    }
+
+}
---
+++ src/main/java/uk/co/caprica/vlcj/enums/ThumbnailerSeekSpeed.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2019 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Enumeration of picture types.
+ */
+public enum ThumbnailerSeekSpeed {
+
+    PRECISE(0),
+    FAST   (1);
+
+    private static final Map<Integer, ThumbnailerSeekSpeed> INT_MAP = new HashMap<Integer, ThumbnailerSeekSpeed>();
+
+    static {
+        for(ThumbnailerSeekSpeed event : ThumbnailerSeekSpeed.values()) {
+            INT_MAP.put(event.intValue, event);
+        }
+    }
+
+    public static ThumbnailerSeekSpeed pictureType(int intValue) {
+        return INT_MAP.get(intValue);
+    }
+
+    private final int intValue;
+
+    ThumbnailerSeekSpeed(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int intValue() {
+        return intValue;
+    }
+
+}
