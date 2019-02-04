e9dcdf3c88709e67bf66c729ea13eb3e2682ec2c
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-04 20:59:12 +0000

    Remove logging from the bespoke Linux initialisation
    Rationale is firstly, errors simply should not happen due to the
    type of initialisation code in this class. Secondly, even if errors
    do occur, we can not do anything about them. This is something that
    at run-time either works or doesn't work.
    
    If there are errors, we robustly ignore them and try and continue
    regardless.

--- src/main/java/uk/co/caprica/vlcj/factory/LinuxNativeInit.java
+++ src/main/java/uk/co/caprica/vlcj/factory/LinuxNativeInit.java
@@ -19,48 +19,54 @@
 
 package uk.co.caprica.vlcj.factory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.co.caprica.vlcj.binding.LibX11;
 
 import javax.swing.*;
 import java.awt.*;
 
+// FIXME if i remove logging, i should at least provide a list of diagnostics messages?
+
 /**
  * Private helper class to ensure the native libraries are properly initialised on Linux.
+ * <p>
+ * If there are any errors when executing this initialisation code there is little point in even reporting them as
+ * nothing else can be done. The approach therefore is do a best-effort at running this initialisation and robustly
+ * ignore errors.
+ * <p>
+ * The reality is that the nature of the initialisation code means errors simply should not occur.
  */
 final class LinuxNativeInit {
 
-    private static final Logger logger = LoggerFactory.getLogger(LinuxNativeInit.class);
-
     static void init() {
+        initAWT();
+        initX();
+    }
+
+    private static void initAWT() {
         if (!GraphicsEnvironment.isHeadless()) {
-            // Only apply if the run-time version is Java 1.7.0 or later...
-            logger.debug("Trying workaround for Java7+ on Linux");
             try {
-                logger.debug("Attempting to load jawt...");
-                // To prevent crashses in some applications, we must seemingly make sure that Swing is
-                // initialised before force-loading libjawt - empirically both of these things are required
+                // To prevent crashes in some applications, we must seemingly make sure that Swing is initialised before
+                // force-loading libjawt - empirically both of these things are required
                 new JPanel();
                 System.loadLibrary("jawt");
-                logger.debug("...loaded jawt");
             }
-            catch(UnsatisfiedLinkError e) {
-                logger.debug("Failed to load jawt", e);
+            catch (UnsatisfiedLinkError e) {
             }
-            logger.debug("Java7 on Linux workaround complete.");
         }
-        // With recent VLC/JDK it seems necessary to do this - it can however cause problems if using the JVM
-        // splash-screen options. Without this, VLC may complain to the console output and it is also possible that
-        // opening a JavaFX FileChooser will cause a fatal JVM crash
+    }
+
+    /**
+     * With recent VLC/JDK it seems necessary to do this - it can however cause problems if using the JVM splash-screen
+     * options. Without this, VLC may complain to the console output and it is also possible that opening a JavaFX
+     * FileChooser will cause a fatal JVM crash.
+     */
+    private static void initX() {
         String initX = System.getProperty("VLCJ_INITX");
-        logger.debug("initX={}", initX);
         if (!"no".equalsIgnoreCase(initX)) {
             try {
                 LibX11.INSTANCE.XInitThreads();
             }
             catch (Exception e) {
-                logger.warn("XInitThreads failed", e);
             }
         }
     }
