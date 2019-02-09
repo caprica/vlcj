9300c48802c3bead2f1a45bccfb13f19c9e1d387
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-09 20:55:29 +0000

    Javadoc for discovery packages

--- src/main/java/uk/co/caprica/vlcj/discovery/package-info.java
+++ src/main/java/uk/co/caprica/vlcj/discovery/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Provides components that can be used to automatically discover the location
- * of the libvlc native libraries in the absence of proper run-time environment
- * configuration.
+ * Provides components that can be used to automatically discover the location of the LibVLC native libraries in the
+ * absence of proper run-time environment configuration.
  */
 package uk.co.caprica.vlcj.discovery;
---
+++ src/main/java/uk/co/caprica/vlcj/discovery/provider/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Native discovery strategy implementation that searches well-known directories to find native libraries.
+ * <p>
+ * Components that provide the directories to search are loaded at run-time by the {@link java.util.ServiceLoader}
+ * mechanism.
+ */
+package uk.co.caprica.vlcj.discovery.provider;
