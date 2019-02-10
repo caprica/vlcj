b0214b325cabde6a7488ef50017a0edbbb4be820
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-10 20:01:39 +0000

    Update package Javadoc

--- src/main/java/uk/co/caprica/vlcj/factory/package-info.java
+++ src/main/java/uk/co/caprica/vlcj/factory/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Media player factory.
+ * <p>
+ * The factory is likely the first component client applications will use, it loads the native library and creates new
+ * media player components,
  */
 package uk.co.caprica.vlcj.factory;
