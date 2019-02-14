c23fbc30bda6d636ee583cab191a30dc992e2674
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-14 11:55:02 +0000

    Update Javadoc for the eventmanager package

---
+++ src/main/java/uk/co/caprica/vlcj/eventmanager/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Base implementation for components that use a native event manager.
+ * <p>
+ * <em>This package is <strong>not</strong> public API.</em>
+ */
+package uk.co.caprica.vlcj.eventmanager;
