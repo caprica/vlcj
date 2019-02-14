9c5df5ce30775feeb48d27c5bd74dbc0ee9923dd
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-02-14 11:39:46 +0000

    Update Javadoc for the project root package

--- src/main/java/uk/co/caprica/vlcj/package-info.java
+++ src/main/java/uk/co/caprica/vlcj/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Java framework for the VLC media player.
+ * <p>
+ * Most applications should start with the {@link uk.co.caprica.vlcj.factory.MediaPlayerFactory}.
  */
 package uk.co.caprica.vlcj;
