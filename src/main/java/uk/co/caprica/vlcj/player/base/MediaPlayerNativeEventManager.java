5480b2c47aaa0d0fbb3771017b5e5e01dca923ed
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-07-13 08:20:41 +0100

    Change media player event manager to suppress spurious stopped events
    vlcj 4.x introduced behaviour (related to media with sub-items, like
    YouTube media) whereby an associated media list player was always
    explicitly stopped when changing media. An unintended side-effect of
    this was for a spurious media player "stopped" event to be raised
    before the media was played.
    
    Getting a stopped event before a played event is undesirable and
    confusing behaviour.
    
    This commit adds a mechanism to suppress the generation of this
    stopped event - now, each time media is changed, a stopped event will
    be suppressed unless there was a played event generated prior. The
    mechanism will be reset each time the media is changed.
    
    This should have no effect where media is played/stopped/played etc.

--- src/main/java/uk/co/caprica/vlcj/player/base/MediaPlayerNativeEventManager.java
+++ src/main/java/uk/co/caprica/vlcj/player/base/MediaPlayerNativeEventManager.java
@@ -23,13 +23,36 @@ import uk.co.caprica.vlcj.binding.internal.libvlc_event_e;
 import uk.co.caprica.vlcj.binding.internal.libvlc_event_manager_t;
 import uk.co.caprica.vlcj.binding.internal.libvlc_event_t;
 import uk.co.caprica.vlcj.binding.internal.libvlc_instance_t;
+import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
+import uk.co.caprica.vlcj.player.base.events.MediaPlayerEvent;
 import uk.co.caprica.vlcj.player.base.events.MediaPlayerEventFactory;
 import uk.co.caprica.vlcj.support.eventmanager.EventNotification;
 import uk.co.caprica.vlcj.support.eventmanager.NativeEventManager;
 
 import static uk.co.caprica.vlcj.binding.LibVlc.libvlc_media_player_event_manager;
 
+/**
+ * Native event manager implementation for media player events.
+ * <p>
+ * There is some special case handling in {@link #onCreateEvent(libvlc_instance_t, libvlc_event_t, MediaPlayer)} to deal
+ * with side-effects when changing media where we invoke stop on the media list player associated with the media player
+ * instance. This occurs in {@link SubitemApi#changeMedia(libvlc_media_t)} and is required for proper operation of media
+ * that creates sub-items (e.g. YouTube or similar). Without this stop call, the sub-items would not play properly for
+ * such media.
+ * <p>
+ * The side-effect is that the media player will receive a spurious  media player stopped event, before the media has
+ * even started playing. This is unexpected behaviour and is likely to be confusing to client applications.
+ * <p>
+ * The special case handling itself suppresses this spurious media player stopped event if a playing event has not been
+ * received prior. The heuristic resets each time the media changes. This issue is a specific consequence of handling
+ * a change in media.
+ * <p>
+ * This class is the appropriate place for the implementation as this code will always be called on the native media
+ * player event thread and will never be invoked in parallel oor by any other thread.
+ */
 final class MediaPlayerNativeEventManager extends NativeEventManager<MediaPlayer, MediaPlayerEventListener> {
+
+    private boolean receivedPlayingEvent;
 
     MediaPlayerNativeEventManager(libvlc_instance_t libvlcInstance, MediaPlayer eventObject) {
         super(libvlcInstance, eventObject, libvlc_event_e.libvlc_MediaPlayerMediaChanged, libvlc_event_e.libvlc_MediaPlayerChapterChanged, "media-player-events");
@@ -42,6 +65,23 @@ final class MediaPlayerNativeEventManager extends NativeEventManager<MediaPlayer
 
     @Override
     protected EventNotification<MediaPlayerEventListener> onCreateEvent(libvlc_instance_t libvlcInstance, libvlc_event_t event, MediaPlayer eventObject) {
+        switch (libvlc_event_e.event(event.type)) {
+            case libvlc_MediaPlayerMediaChanged:
+                // Reset state each time the media changes, subsequent plays and stops are fine
+                receivedPlayingEvent = false;
+                break;
+            case libvlc_MediaPlayerPlaying:
+                // Playing event was received
+                receivedPlayingEvent = true;
+                break;
+            case libvlc_MediaPlayerStopped:
+                // We do not went to send a stopped event if we never received a playing event
+                if (!receivedPlayingEvent) {
+                    // Return null here to suppress the event
+                    return null;
+                }
+                break;
+        }
         return MediaPlayerEventFactory.createEvent(libvlcInstance, eventObject, event);
     }
 
