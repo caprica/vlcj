1006cd5ddb6508b398a30f5ebdbd46daea4d9135
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-04-12 11:04:23 +0100

    Module system changes
    Still experimenting with the Java Module System, declare the native
    discovery services with "uses".

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module uk.co.caprica.vlcj {
     requires jdk.unsupported;
     requires com.sun.jna;
     requires com.sun.jna.platform;
+
     exports uk.co.caprica.vlcj.factory;
     exports uk.co.caprica.vlcj.factory.discovery;
     exports uk.co.caprica.vlcj.factory.discovery.provider;
@@ -44,4 +45,6 @@ module uk.co.caprica.vlcj {
     exports uk.co.caprica.vlcj.waiter;
     exports uk.co.caprica.vlcj.waiter.media;
     exports uk.co.caprica.vlcj.waiter.mediaplayer;
+
+    uses uk.co.caprica.vlcj.factory.discovery.provider.DiscoveryDirectoryProvider;
 }
