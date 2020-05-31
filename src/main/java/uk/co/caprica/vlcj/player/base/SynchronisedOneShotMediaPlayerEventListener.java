89263ec018e87fe9f727bd14dc72240a19b5c4a5
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-05-31 08:54:52 +0100

    Add a synchronised one-shot media player event listener
    The listener has a mechanism to remove itself after one (or more)
    particular events have fired, and to decrement a supplied
    CountdownLatch on completion.

---
+++ src/main/java/uk/co/caprica/vlcj/player/base/SynchronisedOneShotMediaPlayerEventListener.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2020 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.player.base;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Implementation of a {@link OneShotMediaPlayerEventListener} that decrements a {@link CountDownLatch} on completion.
+ * <p>
+ * This can be used e.g. to wait one time for a particular media player event to occur.
+ */
+public abstract class SynchronisedOneShotMediaPlayerEventListener extends OneShotMediaPlayerEventListener {
+
+    /**
+     * Completion synchronisation latch.
+     */
+    private final CountDownLatch latch;
+
+    /**
+     * Create an event listener.
+     *
+     * @param latch completion synchronisation latch
+     */
+    public SynchronisedOneShotMediaPlayerEventListener(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    protected void onDone(MediaPlayer mediaPlayer) {
+        latch.countDown();
+    }
+}
