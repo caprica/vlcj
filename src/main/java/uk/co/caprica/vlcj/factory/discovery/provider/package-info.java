cda610facdac03645057d52c7383562b863ddd17
Mark Lee <mark.lee@capricasoftware.co.uk>
2021-07-17 07:48:40 +0100

    Fix package name in Javadoc

--- src/main/java/uk/co/caprica/vlcj/factory/discovery/provider/package-info.java
+++ src/main/java/uk/co/caprica/vlcj/factory/discovery/provider/package-info.java
@@ -6,7 +6,7 @@
  * <p>
  * Applications can easily provide their own {@link uk.co.caprica.vlcj.factory.discovery.provider.DiscoveryDirectoryProvider}
  * implementations and register the fully-qualified classnames of those implementations in the services descriptor file
- * located at <code>META-INF/services/uk.co.caprica.vlcj.discovery.provider.DiscoveryDirectoryProvider</code> (in their
+ * located at <code>META-INF/services/uk.co.caprica.vlcj.factory.discovery.provider.DiscoveryDirectoryProvider</code> (in their
  * own application classpath).
  */
 package uk.co.caprica.vlcj.factory.discovery.provider;
