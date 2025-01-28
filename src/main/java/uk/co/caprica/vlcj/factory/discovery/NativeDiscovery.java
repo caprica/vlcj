f23e689830cc2a207e40eb041613512dee1cfbca
Mark Lee <mark.lee@capricasoftware.co.uk>
2025-01-28 22:14:47 +0000

    Set string encoding for libvlc library
    For Linux, everything just works, but for Windows it is necessary
    to set the string encoding to UTF-8 so that strings passed to
    LibVLC can be properly handled (e.g. when they contain UNICODE
    characters).

--- src/main/java/uk/co/caprica/vlcj/factory/discovery/NativeDiscovery.java
+++ src/main/java/uk/co/caprica/vlcj/factory/discovery/NativeDiscovery.java
@@ -1,5 +1,6 @@
 package uk.co.caprica.vlcj.factory.discovery;
 
+import com.sun.jna.Library;
 import com.sun.jna.NativeLibrary;
 import com.sun.jna.StringArray;
 import uk.co.caprica.vlcj.binding.support.runtime.RuntimeUtil;
@@ -11,6 +12,7 @@ import uk.co.caprica.vlcj.factory.discovery.strategy.NativeDiscoveryStrategy;
 import uk.co.caprica.vlcj.factory.discovery.strategy.WindowsNativeDiscoveryStrategy;
 import uk.co.caprica.vlcj.support.version.LibVlcVersion;
 
+import static java.util.Collections.singletonMap;
 import static uk.co.caprica.vlcj.binding.lib.LibVlc.libvlc_new;
 import static uk.co.caprica.vlcj.binding.lib.LibVlc.libvlc_release;
 
@@ -113,6 +115,7 @@ public class NativeDiscovery {
                     if (path != null) {
                         if (discoveryStrategy.onFound(path)) {
                             NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), path);
+                            NativeLibrary.getInstance(RuntimeUtil.getLibVlcLibraryName(), singletonMap(Library.OPTION_STRING_ENCODING, "UTF-8"));
                         }
                         tryPluginPath(path, discoveryStrategy);
                         if (tryLoadingLibrary()) {
